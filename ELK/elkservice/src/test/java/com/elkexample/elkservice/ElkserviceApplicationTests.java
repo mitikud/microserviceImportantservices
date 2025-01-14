04734b687aedaa900f9aa2229edb8ec4a4da4679
mitkiflemariam <mitkiflemariam@gmail.com>
2025-01-14 02:29:27 -0500

    kafka-zpkin-elk

---
+++ ELK/elkservice/src/test/java/com/elkexample/elkservice/ElkserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elkexample.elkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
